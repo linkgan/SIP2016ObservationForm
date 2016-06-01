95f368ce0d389442870e9a1f206b5c9c105edd7b
Ellen McCullagh <ellenmccullagh@Ellens-MacBook-Air-2.local>
2016-06-01 14:06:41 -0700

    Initial Commit

---
+++ js/data.js
@@ -0,0 +1,378 @@
+var classroom_names = [{id: 0, text: 'Accenture Atlanta', speakers: [
+	{id: 0, text: 'Georgia Power'},
+	{id: 1, text: 'Hadiyah-Nicole Green'},
+	{id: 2, text: 'Chae O\'Keefe'},
+	{id: 3, text: 'Clay Nelson'}
+]},
+{id: 1, text: 'AT&T Atlanta: Ada Lovelace', speakers: [
+	{id: 0, text: 'Georgia Power'},
+	{id: 1, text: 'Josie Kressner'},
+	{id: 2, text: 'Titania Jordan'},
+	{id: 3, text: 'Clay Nelson'}
+]},
+{id: 2, text: 'AT&T Atlanta: Grace Hopper', speakers: [
+	{id: 0, text: 'Georgia Power'},
+	{id: 1, text: 'Korhonda Randolph'},
+	{id: 2, text: 'Titania Jordan'},
+	{id: 3, text: 'Clay Nelson'}
+]},
+{id: 3, text: 'GE Atlanta', speakers: [
+	{id: 0, text: 'Atlanta Tech Village'},
+	{id: 1, text: 'Erin Martin'},
+	{id: 2, text: 'Betsy Hare'},
+	{id: 3, text: 'Terri Chu'}
+]},
+{id: 4, text: 'AT&T Austin', speakers: [
+	{id: 0, text: 'UT Austin'},
+	{id: 1, text: 'Capital Factory'},
+	{id: 2, text: 'Anita Johnson'},
+	{id: 3, text: 'Patty Cifra'}
+]},
+{id: 5, text: 'Barlovento Foundation @ Austin Community College', speakers: [
+]},
+{id: 6, text: 'The Barlovento Foundation @ Texas State University at Round Rock', speakers: [
+]},
+{id: 7, text: 'IBM Austin', speakers: [
+	{id: 0, text: 'Capital Factory'},
+	{id: 1, text: 'Patty Cifra'}
+]},
+{id: 8, text: 'Indeed Austin', speakers: [
+	{id: 0, text: 'UT Austin'},
+	{id: 1, text: 'Capital Factory'},
+	{id: 2, text: 'Lyndsey Seale'},
+	{id: 3, text: 'Sina Bahram'},
+	{id: 4, text: 'Ashley Lowry'}
+]},
+{id: 9, text: 'Akamai Boston', speakers: [
+	{id: 0, text: 'Fathom'}
+]},
+{id: 10, text: 'Microsoft Boston', speakers: [
+	{id: 0, text: 'Lauren Metter + Sarah Ribeiro'},
+	{id: 1, text: 'Gabby Raymond'}
+]},
+{id: 11, text: 'TripAdvisor Boston', speakers: [
+	{id: 0, text: 'Mary Marken'}
+]},
+{id: 12, text: 'Twitter Boston', speakers: [
+	{id: 0, text: 'Mojgan Lefevbre'},
+	{id: 1, text: 'Mary Marken'}
+]},
+{id: 13, text: 'Verizon Boston', speakers: [
+	{id: 0, text: 'Mary Marken'},
+	{id: 1, text: 'Lauren Metter + Sarah Ribeiro'}
+]},
+{id: 14, text: 'Accenture Chicago', speakers: [
+	{id: 0, text: 'Groupon'},
+	{id: 1, text: 'Sheri Rubin'}
+]},
+{id: 15, text: 'Girls Who Code @  the University of Illinois at Chicago: Ada Lovelace', speakers: [
+	{id: 0, text: 'Blue1647'},
+	{id: 1, text: 'Star Cunningham'},
+	{id: 2, text: 'Tasha Wilkins'}
+]},
+{id: 16, text: 'Girls Who Code @ University of Illinois at Chicago: Grace Hopper', speakers: [
+	{id: 0, text: 'Microsoft'},
+	{id: 1, text: '1871'},
+	{id: 2, text: 'Star Cunningham'},
+	{id: 3, text: 'Tasha Wilkins'}
+]},
+{id: 17, text: 'Microsoft Chicago', speakers: [
+	{id: 0, text: 'Groupon'},
+	{id: 1, text: 'Adler Planetarium'},
+	{id: 2, text: 'Sheri Rubin'},
+	{id: 3, text: 'Samantha Alaimo'},
+	{id: 4, text: 'Ally Brisbin'}
+]},
+{id: 18, text: 'Synchrony Financial Chicago', speakers: [
+	{id: 0, text: 'Groupon'},
+	{id: 1, text: 'Adler Planetarium'},
+	{id: 2, text: 'Sheri Rubin'},
+	{id: 3, text: 'Praveen Yadav'}
+]},
+{id: 19, text: 'Verizon Chicago', speakers: [
+	{id: 0, text: 'DePaul University Tour'},
+	{id: 1, text: 'Groupon'},
+	{id: 2, text: 'Stella Garber'},
+	{id: 3, text: 'Alison Stanton'}
+]},
+{id: 20, text: 'AT&T Los Angeles', speakers: [
+	{id: 0, text: 'Team One'},
+	{id: 1, text: 'Olivia Orrell-Jones'},
+	{id: 2, text: 'Kimberly Scott'},
+	{id: 3, text: 'Marie Smith'}
+]},
+{id: 21, text: 'Disney Los Angeles', speakers: [
+	{id: 0, text: 'Kayla Briet'},
+	{id: 1, text: 'Kimberly Scott'},
+	{id: 2, text: 'Liliana Monge'}
+]},
+{id: 22, text: 'IBM Los Angeles', speakers: [
+	{id: 0, text: 'Justin Washington'},
+	{id: 1, text: 'Marie Smith'},
+	{id: 2, text: 'Liliana Monge'}
+]},
+{id: 23, text: 'Cheryl Saban Self-Worth Foundation for Women & Girls at USC Marshall School of Business: Ada Lovelace', speakers: [
+	{id: 0, text: 'Kayla Briet'},
+	{id: 1, text: 'Lea Deesing'},
+	{id: 2, text: 'Evette Vargas'},
+	{id: 3, text: 'Lauren Mermel'}
+]},
+{id: 24, text: 'Cheryl Saban Self-Worth Foundation for Women & Girls at USC Marshall School of Business: Grace Hopper', speakers: [
+	{id: 0, text: 'Kayla Briet'},
+	{id: 1, text: 'Lea Deesing'},
+	{id: 2, text: 'Evette Vargas'},
+	{id: 3, text: 'Lauren Mermel'}
+]},
+{id: 25, text: 'Knight Foundation at MDC Miami: Ada Lovelace', speakers: [
+]},
+{id: 26, text: 'Knight Foundation Miami @ FIU', speakers: [
+]},
+{id: 27, text: 'Verizon Newark: Ada Lovelace', speakers: [
+	{id: 0, text: 'NBCUniversal'}
+]},
+{id: 28, text: 'Verizon Newark: Grace Hopper', speakers: [
+	{id: 0, text: 'NBCUniversal'},
+	{id: 1, text: 'Jessica Sheft-Ason'},
+	{id: 2, text: 'Ajaita Saini'}
+]},
+{id: 29, text: 'Accenture New York City', speakers: [
+	{id: 0, text: 'Blippar'},
+	{id: 1, text: 'New York Times'},
+	{id: 2, text: 'Patricia Hernon'},
+	{id: 3, text: 'Lorelle Reid'}
+]},
+{id: 30, text: 'Adobe New York City', speakers: [
+	{id: 0, text: 'Squarespace'},
+	{id: 1, text: 'New York Times'},
+	{id: 2, text: 'Kirti Agarwal'},
+	{id: 3, text: 'Lorelle Reid'}
+]},
+{id: 31, text: 'AIG New York City: Ada Lovelace', speakers: [
+	{id: 0, text: 'TechStars'},
+	{id: 1, text: 'LinkedIn'}
+]},
+{id: 32, text: 'AIG New York City: Grace Hopper', speakers: [
+	{id: 0, text: 'TechStars'},
+	{id: 1, text: 'LinkedIn'}
+]},
+{id: 33, text: 'The AOL Charitable Foundation at the CUNY Advanced Science Research Center', speakers: [
+	{id: 0, text: 'DataKind'},
+	{id: 1, text: 'Adrienne Shulman'}
+]},
+{id: 34, text: 'AppNexus New York City', speakers: [
+	{id: 0, text: 'Samantha Quinones'},
+	{id: 1, text: 'Ajaita Saini'}
+]},
+{id: 35, text: 'AT&T New York City', speakers: [
+	{id: 0, text: 'Chanel'},
+	{id: 1, text: 'Adrienne Shulman'},
+	{id: 2, text: 'Sunny Bates'}
+]},
+{id: 36, text: 'BlackRock New York City', speakers: [
+	{id: 0, text: 'Squarespace'},
+	{id: 1, text: 'Christina Morillo'},
+	{id: 2, text: 'Isabella Frassetti'}
+]},
+{id: 37, text: 'Goldman Sachs New York City: Ada Lovelace', speakers: [
+	{id: 0, text: 'Refinery 29'},
+	{id: 1, text: 'Amy Quispe'},
+	{id: 2, text: 'Caroline Sinders'}
+]},
+{id: 38, text: 'Goldman Sachs New York City: Grace Hopper', speakers: [
+	{id: 0, text: 'Refinery 29'},
+	{id: 1, text: 'Amy Quispe'},
+	{id: 2, text: 'Caroline Sinders'}
+]},
+{id: 39, text: 'IAC New York City', speakers: [
+	{id: 0, text: 'Squarespace'},
+	{id: 1, text: 'Francesca Colombo'},
+	{id: 2, text: 'Amy Quispe'}
+]},
+{id: 40, text: 'IBM New York City', speakers: [
+	{id: 0, text: 'News Corp'},
+	{id: 1, text: 'Tiffany Xiao'}
+]},
+{id: 41, text: 'JPMorgan Chase New York City', speakers: [
+	{id: 0, text: 'Tiffany Xiao'},
+	{id: 1, text: 'Stevie Aligbe'}
+]},
+{id: 42, text: 'Kate Spade & Company @ the Fashion Institute of Technology', speakers: [
+	{id: 0, text: 'New York Times'},
+	{id: 1, text: 'Rebecca Garcia'},
+	{id: 2, text: 'Lorelle Reid'},
+	{id: 3, text: 'Lindsay Hall'}
+]},
+{id: 43, text: 'Moody\'s New York City', speakers: [
+	{id: 0, text: 'Fueled Collective'},
+	{id: 1, text: 'Christina Morillo'}
+]},
+{id: 44, text: 'Microsoft New York City', speakers: [
+	{id: 0, text: 'New York Times'},
+	{id: 1, text: 'Stevie Aligbe'}
+]},
+{id: 45, text: 'Viacom New York City', speakers: [
+	{id: 0, text: 'News Corp'},
+	{id: 1, text: 'Rebecca Garcia'},
+	{id: 2, text: 'Kelly Moran'},
+	{id: 3, text: 'Nathalie Leon'}
+]},
+{id: 46, text: 'Adobe Seattle', speakers: [
+	{id: 0, text: 'Gabrielle Gutierrez'}
+]},
+{id: 47, text: 'Amazon Seattle', speakers: [
+	{id: 0, text: 'Gabrielle Gutierrez'}
+]},
+{id: 48, text: 'AT&T Seattle', speakers: [
+	{id: 0, text: 'Seattle Times'},
+	{id: 1, text: 'Gabrielle Gutierrez'}
+]},
+{id: 49, text: 'Expedia Seattle', speakers: [
+	{id: 0, text: 'Anna Marie Golden'},
+	{id: 1, text: 'Mel Carson'}
+]},
+{id: 50, text: 'Groupon Seattle', speakers: [
+	{id: 0, text: 'Seattle Times'},
+	{id: 1, text: 'Pivotal Labs'}
+]},
+{id: 51, text: 'Microsoft Seattle', speakers: [
+	{id: 0, text: 'Seattle Times'},
+	{id: 1, text: 'Anna Marie Golden'}
+]},
+{id: 52, text: 'Adobe San Jose: Ada Lovelace', speakers: [
+	{id: 0, text: 'Yahoo!'},
+	{id: 1, text: 'Kirsten Wolberg'},
+	{id: 2, text: 'Diana Navarro'}
+]},
+{id: 53, text: 'Adobe San Jose: Grace Hopper', speakers: [
+	{id: 0, text: 'Yahoo!'},
+	{id: 1, text: 'Kirsten Wolberg'},
+	{id: 2, text: 'Diana Navarro'}
+]},
+{id: 54, text: 'Adobe San Francisco', speakers: [
+	{id: 0, text: 'Pinterest'},
+	{id: 1, text: 'Alyssa Simpson'}
+]},
+{id: 55, text: 'Amazon Bay Area', speakers: [
+	{id: 0, text: 'Lifelock/Stanford'},
+	{id: 1, text: 'Tejas Manohar'},
+	{id: 2, text: 'Paula Griffin'},
+	{id: 3, text: 'Diana Navarro'}
+]},
+{id: 56, text: 'Autodesk San Francisco', speakers: [
+	{id: 0, text: 'Aminatou Sow'}
+]},
+{id: 57, text: 'Electronic Arts Bay Area', speakers: [
+	{id: 0, text: 'Kimberly Unger'},
+	{id: 1, text: 'Kaye Mason'}
+]},
+{id: 58, text: 'Facebook Bay Area', speakers: [
+	{id: 0, text: 'EA'},
+	{id: 1, text: 'Robin Yang'},
+	{id: 2, text: 'Tejas Manohar'},
+	{id: 3, text: 'Paula Griffin'},
+	{id: 4, text: 'Yun Mou'},
+	{id: 5, text: 'Andrea Ayres'}
+]},
+{id: 59, text: 'GE Bay Area', speakers: [
+]},
+{id: 60, text: 'IBM San Jose', speakers: [
+	{id: 0, text: 'Lifelock/Stanford'},
+	{id: 1, text: 'Cecile Cruz'},
+	{id: 2, text: 'Diana Navarro'}
+]},
+{id: 61, text: 'Intuit Bay Area', speakers: [
+	{id: 0, text: 'EA'},
+	{id: 1, text: 'Diana Navarro'},
+	{id: 2, text: 'Kaye Mason'}
+]},
+{id: 62, text: 'Moody\'s San Francisco', speakers: [
+	{id: 0, text: 'Pinterest'},
+	{id: 1, text: 'Kathleen Mullaney'},
+	{id: 2, text: 'Brynne Herbert'},
+	{id: 3, text: 'Julia Rhodes Davis'}
+]},
+{id: 63, text: 'Microsoft Bay Area', speakers: [
+	{id: 0, text: 'Lifelock/Stanford'},
+	{id: 1, text: 'EA'},
+	{id: 2, text: 'Robin Yang'},
+	{id: 3, text: 'Andrea Ayres'}
+]},
+{id: 64, text: 'Pivotal Bay Area', speakers: [
+]},
+{id: 65, text: 'Pixar Bay Area', speakers: [
+	{id: 0, text: 'Lifelock/Stanford'},
+	{id: 1, text: 'Dolby'},
+	{id: 2, text: 'Sheryl Kline'}
+]},
+{id: 66, text: 'Prudential Newark', speakers: [
+	{id: 0, text: 'Marlyne Pierce'}
+]},
+{id: 67, text: 'Sephora San Francisco', speakers: [
+	{id: 0, text: 'Airbnb'},
+	{id: 1, text: 'Alyssa Simpson'},
+	{id: 2, text: 'Ariana Mott'}
+]},
+{id: 68, text: 'Square San Francisco', speakers: [
+	{id: 0, text: 'Lifelock/Stanford'},
+	{id: 1, text: 'Dolby'},
+	{id: 2, text: 'Claire Uhar'},
+	{id: 3, text: 'Tasneem Misnadakis'}
+]},
+{id: 69, text: 'Twitter San Francisco', speakers: [
+	{id: 0, text: 'Dolby'},
+	{id: 1, text: 'Claire Uhar'},
+	{id: 2, text: 'Tasneem Misnadakis'}
+]},
+{id: 70, text: 'Workday Bay Area', speakers: [
+	{id: 0, text: 'Radius'}
+]},
+{id: 71, text: 'AOL Washington DC', speakers: [
+	{id: 0, text: '1776'},
+	{id: 1, text: 'Lisa Hines'},
+	{id: 2, text: 'Rhianon Anderson'}
+]},
+{id: 72, text: 'AT&T Washington DC', speakers: [
+	{id: 0, text: 'Palantir'},
+	{id: 1, text: 'Shannon Turner'},
+	{id: 2, text: 'Rhianon Anderson'},
+	{id: 3, text: 'Kelli Kedis Ogborn'}
+]},
+{id: 73, text: 'BSA Washington DC', speakers: [
+	{id: 0, text: 'Reshma/Megan Smith Town Hall'},
+	{id: 1, text: 'Lisa O\'Brien'},
+	{id: 2, text: 'Shannon Turner'},
+	{id: 3, text: 'Kelli Kedis Ogborn'}
+]},
+{id: 74, text: 'Capital One Washington DC', speakers: [
+	{id: 0, text: 'Reshma/Megan Smith Town Hall'},
+	{id: 1, text: '1776'},
+	{id: 2, text: 'Lisa Hines'},
+	{id: 3, text: 'Rhianon Anderson'}
+]},
+{id: 75, text: 'Knight Foundation at MDC Miami: Grace Hopper', speakers: [
+]},
+{id: 76, text: 'Lockheed Martin Washington DC: Ada Lovelace', speakers: [
+	{id: 0, text: 'Reshma/Megan Smith Town Hall'},
+	{id: 1, text: 'General Assembly'},
+	{id: 2, text: 'Kelli Kedis Ogborn'}
+]},
+{id: 77, text: 'Lockheed Martin Washington DC: Grace Hopper', speakers: [
+	{id: 0, text: 'Reshma/Megan Smith Town Hall'},
+	{id: 1, text: 'General Assembly'},
+	{id: 2, text: 'Shannon Turner'}
+]},
+  { id: 78, text: 'Select a Classroom'}
+]
+
+var surveysData = [ //Codes are for form assembly forms in order classroom, first name, last name
+    { id: 0, text: "Pre-Program Survey", url: "https://www.tfaforms.com/424136", codes: ["tfa_297", "tfa_695", "tfa_988"]},
+    { id: 1, text: 'Pre Evaluation', url: 'https://www.tfaforms.com/424137', codes: ["tfa_122", "tfa_124", "tfa_126"]},
+    { id: 2, text: 'Week 1 Check-in', url: "https://www.tfaforms.com/424576", codes: ["tfa_297", "tfa_695", "tfa_989"]},
+    { id: 3, text: 'Week 2 Check-in', url: "https://www.tfaforms.com/424585", codes: ["tfa_297", "tfa_695", "tfa_988"]},
+    { id: 4, text: 'Week 3 Check-in', url: "https://www.tfaforms.com/424572", codes: ["tfa_297", "tfa_695", "tfa_1028"]},
+    { id: 5, text: "Mid-Program Survey", url: "https://www.tfaforms.com/424584", codes: ["tfa_297", "tfa_695", "tfa_988"]},
+    { id: 6, text: 'Week 4 Check-in', url: "https://www.tfaforms.com/424574", codes: ["tfa_297", "tfa_695", "tfa_697"]},
+    { id: 7, text: 'Post Evaluation', url: "https://www.tfaforms.com/424578", codes: ["tfa_256", "tfa_258", "tfa_260"]},
+    { id: 8, text: 'Post-Program Survey', url: "https://www.tfaforms.com/424586", codes: ["tfa_297", "tfa_695", "tfa_1028"]},
+    { id: 9, text: 'Speaker & Field Trip Feedback', url: 'pe.html', codes: ["class_id", "first_name", "last_name"]}
+]
